f1f5e67714b5af756f04cabe974a3ce7807eb6d2
hyeryoung <kimhyeryoung@gimhyelyeong-ui-MacBookPro.local>
2021-06-08 21:38:18 +0900

    Second commit

---
+++ Section06/Array.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Section06
+{
+    /*
+    class Player
+    {
+
+    }
+
+    class Monster
+    {
+
+    }
+    */
+
+    class Array
+    {
+        /*
+        Player player;
+        Monster monster;
+        Monster monster_2;
+        Monster monster_3;
+        */
+
+        //최대값
+        static int GetHighScore(int[] scores)
+        {
+            int maxValue = 0;
+
+            foreach (int score in scores)
+            {
+                if (score > maxValue)
+                    maxValue = score;
+            }
+
+            return maxValue;
+        }
+
+        //평균
+        static int GetAverageSore(int[] scores)
+        {
+            if (scores.Length == 0)
+                return 0;
+
+            int sum = 0;
+
+            foreach ( int score in scores)
+            {
+                sum += score;
+            }
+            
+            return sum / scores.Length;
+        }
+
+        //인덱스값
+        static int GetIndexOf(int[] scores, int value)
+        {
+            //인덱스 반환이 목표이니 굳이 foreach를 쓸 필요 없이 for문으로 0번부터 인덱스 탐색
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] == value)
+                    return i;
+            }
+
+            return -1; //값을 찾지 못했을 떄 -1 반환
+        }
+
+        //정렬
+        static void Sort(int[] scores)
+        {
+            for (int i = 0; i < scores.Length; i++)
+            {
+                // [i ~ sccores.Length - 1]에서 제일 작은 숫자가 있는 inex를 찾는다.
+                int minIndex = i;
+                for (int j = i;  j < scores.Length; j++)
+                {
+                    if (scores[j] < scores[minIndex])
+                        minIndex = j;
+                }
+
+                // swap
+                int temp = scores[i];
+                scores[i] = scores[minIndex];
+                scores[minIndex] = temp;
+
+                
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            /*
+            //배열
+            int[] scores = new int[5] { 10, 20, 30, 40, 50 };
+            int[] scores2 = scores;
+
+            scores2[0] = 9999; //scores의 첫번째 인자인 10이 9999로 변경됨
+
+            //0 1 2 3 4
+            //scores[0] = 10;
+            //scores[1] = 20;
+            //scores[2] = 30;
+            //scores[3] = 40;
+            //scores[4] = 50;
+
+            //for문
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
+
+            //foreach문
+            foreach (int score in scores)
+            {
+                Console.WriteLine(score);
+            }*/
+            
+            
+            
+            //연습문제
+            int[] scores = new int[5] { 10, 30, 40, 20, 50 };
+
+            int highestScore = GetHighScore(scores);
+            Console.WriteLine(highestScore);
+
+            int averageScore = GetAverageSore(scores);
+            Console.WriteLine(averageScore);
+
+            int index = GetIndexOf(scores, 20);
+            Console.WriteLine(index);
+
+            Sort(scores); // 10 20 30 40 50 순서로 정렬
+        
+        }
+    
+    }
+}
---
+++ Section06/MultiArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Section06
+{
+    public class MultiArray
+    {
+        //다차원 배열
+        
+        class Map
+        {
+            //[n,m]
+            int[,] tiles =
+            {
+                {1,1,1,1,1},
+                {1,0,0,0,1},
+                {1,0,0,0,1},
+                {1,0,0,0,1},
+                {1,1,1,1,1},
+
+            };
+
+            public void Render()
+            {
+                //Array.GetLength(i) : 배열의 i차원의 크기를 제공, 0차원부터 시작한다(1차원: 0 2차원: 1)
+                //1차원 배열의 길이 n 2차원 배열의 길이 m
+                //Array.Length를 사용하면 nm의 값을 얻는다.
+                for (int y = 0; y < tiles.GetLength(1); y++)//GetLength(1) : m
+                {
+                    for (int x = 0; x < tiles.GetLength(0); x++)//GetLength(0) : n
+                    {
+                        if (tiles[x, y] == 1)
+                            Console.ForegroundColor = ConsoleColor.Red;// 색깔변경
+                        else
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write('\u25cf');//동그라미출력 
+                    }
+                    Console.WriteLine();//개행 
+                }
+            }
+        }
+
+       
+        /*
+        static void Main(string[] args)
+        {
+            Map map = new Map();
+            map.Render();
+        }
+        */
+    }
+}
